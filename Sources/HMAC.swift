1ecb52ff4a7eb400f7152a44d7f3fea2f131c8ba
Joannis Orlandos <joannis@orlandos.nl>
2016-04-18 12:56:53 +0200

    Added original copyright

--- Sources/HMAC.swift
+++ Sources/HMAC.swift
@@ -1,10 +1,14 @@
+// Originally based on CryptoSwift by Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
+// Copyright (C) 2014 Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
+// This software is provided 'as-is', without any express or implied warranty.
 //
-//  HMAC.swift
-//  CryptoSwift
+// In no event will the authors be held liable for any damages arising from the use of this software.
 //
-//  Created by Marcin Krzyzanowski on 13/01/15.
-//  Copyright (c) 2015 Marcin Krzyzanowski. All rights reserved.
+// Permission is granted to anyone to use this software for any purpose,including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
 //
+// - The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation is required.
+// - Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+// - This notice may not be removed or altered from any source or binary distribution.
 
 @_exported import CryptoEssentials
 
